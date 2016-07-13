e4e24e5f9eeca9fc407c26997bccb321aa661a8f
Thomas Moore <tomo@pagu.co>
2016-07-13 22:09:13 +0100

    Removed the time background switch

--- src/jsx/components/time.jsx
+++ src/jsx/components/time.jsx
@@ -1,28 +1,9 @@
 import React from 'react';
 import moment from 'moment';
-import { StyleSheet } from 'react-look';
 
 const state = {
 	time: moment().format('MMMM Do YYYY, h:mm:ss a'),
 };
-
-const hour = moment().hour();
-let timeOfDay;
-if (hour >= 5 && hour < 11) {
-	timeOfDay = 'morning';
-} else if (hour >= 11 && hour < 17) {
-	timeOfDay = 'day';
-} else if (hour >= 17 && hour < 20) {
-	timeOfDay = 'evening';
-} else {
-	timeOfDay = 'night';
-}
-
-StyleSheet.toCSS({
-	body: {
-		background: `url("https://source.unsplash.com/featured/1920x1080/?${timeOfDay}") fixed 0/cover no-repeat transparent`,
-	},
-});
 
 class Time extends React.Component {
 	constructor(props) {
