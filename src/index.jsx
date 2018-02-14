f361d39598372248c098b32b13a9c0e5d645bc7b
Thomas Moore <thomas.moore@reedonline.co.uk>
2018-02-14 15:26:40 +0000

    Convert base index.js to index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,26 @@
-import registerServiceWorker from './registerServiceWorker';
-
 import React from 'react';
 import { render } from 'react-dom';
 import { compose, createStore } from 'redux';
 import { Provider } from 'react-redux';
 import persistState from 'redux-localstorage';
-import reducer from './reducers';
 
 import 'normalize.css';
 import { Provider as RebassProvider } from 'rebass';
+
+import reducer from './reducers';
+import registerServiceWorker from './registerServiceWorker';
+
 import App from './components/App';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducer, composeEnhancers(
-  persistState(null, {
-    key: 'starttab',
-  }),
-));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // eslint-disable-line
+const store = createStore(
+  reducer,
+  composeEnhancers(
+    persistState(null, {
+      key: 'starttab',
+    })
+  )
+);
 
 render(
   <Provider store={store}>
