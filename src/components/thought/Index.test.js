34da39929054bad085d401dcb5d27d5477768b0f
Thomas Moore <tomo@uchuu.io>
2018-10-06 13:03:13 +0100

    More tests!

--- src/components/thought/Index.test.js
+++ src/components/thought/Index.test.js
@@ -1,12 +1,40 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 
 import { ThoughtComponent } from './Index';
 
 describe('<Thought />', () => {
-  const props = { modules: {}, thought: {} };
+  const mockFetchLatestThought = jest.fn();
+  let props = { modules: { thought: true }, thought: {}, fetchLatestThought: mockFetchLatestThought };
+
+  let thought = shallow(<ThoughtComponent {...props} />);
 
   it('should render', () => {
-    expect(shallow(<ThoughtComponent {...props} />).is('.thought')).toBe(true);
+    expect(thought.is('.thought')).toBe(true);
+  });
+
+  describe('if nextFetch has not passed', () => {
+    beforeEach(() => {
+      props.thought.nextFetch = new Date();
+
+      thought = mount(<ThoughtComponent {...props} />);
+    });
+
+    it('should not call fetchLatestThought', () => {
+      expect(mockFetchLatestThought).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('if nextFetch has passed', () => {
+    beforeEach(() => {
+      props.thought.isFetching = false;
+      props.thought.lastFetched = '';
+
+      thought = mount(<ThoughtComponent {...props} />);
+    });
+
+    it('should call fetchLatestThought', () => {
+      expect(mockFetchLatestThought).toHaveBeenCalled();
+    });
   });
 });
