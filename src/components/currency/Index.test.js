1b2ba611539a5d54e21a2cd43d9302fc03fdd07c
Thomas Moore <tomo@uchuu.io>
2018-10-06 13:58:22 +0100

    Update currency tests

--- src/components/currency/Index.test.js
+++ src/components/currency/Index.test.js
@@ -1,12 +1,44 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 
 import { CurrencyComponent } from './Index';
 
-describe('<Currency />', () => {
-  const props = { currency: { base: 'GBP', target: 'JPY', rates: { JPY: 150 } }, modules: {} };
+describe('<Thought />', () => {
+  const mockfetchLatestCurrency = jest.fn();
+  let props = {
+    modules: { currency: true },
+    currency: { base: 'GBP', target: 'JPY', rates: { JPY: 150 } },
+    fetchLatestCurrency: mockfetchLatestCurrency,
+  };
+
+  let currency = shallow(<CurrencyComponent {...props} />);
 
   it('should render', () => {
-    expect(shallow(<CurrencyComponent {...props} />).is('.currency')).toBe(true);
+    expect(currency.is('.currency')).toBe(true);
+  });
+
+  describe('if nextFetch has not passed', () => {
+    beforeEach(() => {
+      props.currency.nextFetch = new Date();
+
+      currency = mount(<CurrencyComponent {...props} />);
+    });
+
+    it('should not call fetchLatestCurrency', () => {
+      expect(mockfetchLatestCurrency).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('if nextFetch has passed', () => {
+    beforeEach(() => {
+      props.currency.isFetching = false;
+      props.currency.lastFetched = '';
+
+      currency = mount(<CurrencyComponent {...props} />);
+    });
+
+    it('should call fetchLatestCurrency', () => {
+      expect(mockfetchLatestCurrency).toHaveBeenCalled();
+    });
   });
 });
